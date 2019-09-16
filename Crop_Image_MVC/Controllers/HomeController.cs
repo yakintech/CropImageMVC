f07a904012d6433dac3cddc7a0530d8c8c5f82fa
yakintech <yildiz.cagatay@hotmail.com>
2019-09-16 10:35:07 +0300

    Add project files.

---
+++ Crop_Image_MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crop_Image_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(string imgCropped)
+        {
+            string base64 = Request.Form["imgCropped"];
+            byte[] bytes = Convert.FromBase64String(base64.Split(',')[1]);
+            using (FileStream stream = new FileStream(Server.MapPath("~/Images/Cropped.png"), FileMode.Create))
+            {
+                stream.Write(bytes, 0, bytes.Length);
+                stream.Flush();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Crop_Image_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crop_Image_MVC.MvcApplication" Language="C#" %>
